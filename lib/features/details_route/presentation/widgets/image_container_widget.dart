23d26f955311e74e94684f63aa4edd0f3dfcc187
Роман Алла <romanalla@MacBook-Air-Roman.local>
2025-09-20 21:01:29 +0700

    добавил недостающий виджет

---
+++ lib/features/details_route/presentation/widgets/image_container_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ImageContainerWidget extends StatelessWidget {
+  final String url;
+  const ImageContainerWidget({super.key, required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Image.network(
+        url,
+        fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+          return Container(
+            height: 200,
+            color: Colors.grey[50],
+            child: Center(
+              child: CircularProgressIndicator(
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes!
+                    : null,
+                color: Colors.grey[600],
+              ),
+            ),
+          );
+        },
+        errorBuilder: (context, error, stackTrace) {
+          return Container(
+            height: 200,
+            color: Colors.grey[50],
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.error_outline, color: Colors.grey[400], size: 32),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Ошибка загрузки изображения',
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
